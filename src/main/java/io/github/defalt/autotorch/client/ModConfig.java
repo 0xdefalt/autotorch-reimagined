5ef550a789c3b275b64acb50b41dabee1153d0e8
0xdefalt <erick_nara@icloud.com>
2025-10-25 13:58:15 -0300

    fix: comments

--- src/main/java/io/github/defalt/autotorch/client/ModConfig.java
+++ src/main/java/io/github/defalt/autotorch/client/ModConfig.java
@@ -7,13 +7,16 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config(name = "autotorch-reimagined")
 public class ModConfig implements ConfigData {
 
-    @Comment("Enable the AutoTorch Reimagined mod")
+    @Comment("Enable the AutoTorch Reimagined mod.")
     boolean enabled = true;
 
-    @Comment("Enable accurate torch placement directly on block below.\n" +
-            "Note: This may be considered cheaty because it 'fakes' player rotation.\n" +
-            "And is basically desyncing the client and server\n" +
-            "Enable only if you are sure your server doesn't consider it cheaty.")
+    @Comment("""
+            Enable accurate torch placement directly on block below.
+            Note: This may be considered cheaty because it 'fakes' player rotation
+            and is basically desyncing the client and server.
+            Enable only if you are sure your server doesn't consider it cheaty.
+            """
+    )
     boolean accuratePlacement = false;
 
     @Comment("The block light level below which the torches are placed.")
